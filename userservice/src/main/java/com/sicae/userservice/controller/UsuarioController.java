80a1543471ee401f6623d62bfc4294f1f68d2579
JuanJoMoCa <J_Jose07Morales@outlook.es>
2026-06-17 10:06:04 -0600

    documentacion

--- userservice/src/main/java/com/sicae/userservice/controller/UsuarioController.java
+++ userservice/src/main/java/com/sicae/userservice/controller/UsuarioController.java
@@ -28,16 +28,16 @@ public class UsuarioController {
     //Despues, pasamos al un objeto UsuarioRequestDTO que contiene toda la informacion para crear a nuestro nuevo usuario, este posteriormente se mapeara a un objeto Usuario que es el molde de nuestra base de datos.
     //Finalmente, se hace la autenticacion del token, se llama a usuarioService para que se encargue de aplicar la logica del negocio y finalmente si todo esta bien se realiza la carga a la base de datos y el usuario en postman ve un mensaje de exito, o uno de error en caso de que algo este mal.
     @PostMapping("/agregar")
-    public ResponseEntity<?> agregarNuevoUsuario(@RequestHeader("Authorization") final String authHeader,@RequestBody UsuariosRequestDTO nuevoUsuario) {
+    public ResponseEntity<?> agregarNuevoUsuario(@RequestHeader("Authorization") String tokenR,@RequestBody UsuariosRequestDTO nuevoUsuario) {
         
         try {
             // Primero revisamos si nos mandaron el token en la cabecera
-            if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            if (tokenR == null || !tokenR.startsWith("Bearer ")) {
                 return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Falta el token de autorización o el formato es incorrecto");
             }
             
             // Le quitamos la palabra "Bearer " para dejar el puro token
-            String tokenPuro = authHeader.substring(7);
+            String tokenPuro = tokenR.substring(7);
             
             // Se lo mandamos al servicio junto con los datos que llegaron en el Body
             String mensajeExito = usuarioService.agregarUsuario(tokenPuro, nuevoUsuario);
@@ -56,16 +56,16 @@ public class UsuarioController {
     // solo jala si le pasas estrictamente el token del admin en el header
     // y necesitamos mandarle por la URL el id del usuario que vamos a editar, además del body con los datos nuevos.
     @PutMapping("/editar/{idUsuario}")
-    public ResponseEntity<?> editarUsuario(@RequestHeader("Authorization") final String authHeader, @PathVariable Integer idUsuario, @RequestBody UsuariosRequestDTO nuevoUsuario) {
+    public ResponseEntity<?> editarUsuario(@RequestHeader("Authorization") String tokenR, @PathVariable Integer idUsuario, @RequestBody UsuariosRequestDTO nuevoUsuario) {
 
         try {
             // primero revisamos si nos mandaron el token en la cabecera
-            if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            if (tokenR == null || !tokenR.startsWith("Bearer ")) {
                 return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Falta el token de autorización o el formato es incorrecto");
             }
 
             // Le quitamos la palabra "Bearer " con el substring(7) para dejar el token limpio
-            String tokenPuro = authHeader.substring(7);
+            String tokenPuro = tokenR.substring(7);
 
             // Aquí llamamos a usuarioService, le mandamos el token para validar que sea admin, tambien a quién queremos editar y los datos nuevos.
             // El servicio se encarga de la l[ogica de negocio
@@ -85,16 +85,16 @@ public class UsuarioController {
     // Usamos GetMapping porque solo vamos a consultar datos, no le vamos a mover a nada en la BD.
     // Solo necesitamos el idUsuario en la ruta para saber a quién buscar.
     @GetMapping("/verPerfil/{idUsuario}")
-    public ResponseEntity<?> verPerfil(@RequestHeader("Authorization") final String authHeader,@PathVariable Integer idUsuario) {
+    public ResponseEntity<?> verPerfil(@RequestHeader("Authorization") String tokenR,@PathVariable Integer idUsuario) {
         
         try {
             // Otra vez, verificamos token
-            if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            if (tokenR == null || !tokenR.startsWith("Bearer ")) {
                 return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Falta el token de autorización o el formato es incorrecto");
             }
             
             // Le quitamos la palabra "Bearer " con el substring(7) para dejar el token limpio
-            String tokenPuro = authHeader.substring(7);
+            String tokenPuro = tokenR.substring(7);
             
             //llamamos a UsuarioService para armar el dto con todos los datos del usuario.
             //Le mandamos el token para que valide quién lo está pidiendo y el id del perfil que queremos ver.
@@ -113,16 +113,16 @@ public class UsuarioController {
     //Ultimo endpoint, sirve para actualizar o, bueno, cambiar el estatus de un usuario
     // Necesitamos pedir el id del usuario al que le vamos a mover el estatus, y el id del rol para hacer las validaciones.
     @PatchMapping("/actualizarEstatus/{idUsuario}/{idRol}")
-    public ResponseEntity<?> actualizarEstatus(@RequestHeader("Authorization") final String authHeader,@PathVariable Integer idUsuario, @PathVariable Integer idRol) {
+    public ResponseEntity<?> actualizarEstatus(@RequestHeader("Authorization") String tokenR,@PathVariable Integer idUsuario, @PathVariable Integer idRol) {
 
         try {
             // Otra vez, verificamos token
-            if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            if (tokenR == null || !tokenR.startsWith("Bearer ")) {
                 return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Falta el token de autorización o el formato es incorrecto");
             }
 
             // Le quitamos la palabra "Bearer " con el substring(7) para dejar el token limpio
-            String tokenPuro = authHeader.substring(7);
+            String tokenPuro = tokenR.substring(7);
 
             // Mandamos a llamar a usuarioService para que haga el update en la base.
             String mensajeExito = usuarioService.actualizarEstatus(tokenPuro, idUsuario, idRol);
